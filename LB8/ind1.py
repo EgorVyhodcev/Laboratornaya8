8921f4dfd9bd36a355aede6ae696e41622115dd1
EgorVyhodcev <90092126+EgorVyhodcev@users.noreply.github.com>
2021-10-29 16:28:09 +0300

    fix

--- LB8/ind1.py
+++ LB8/ind1.py
@@ -3,20 +3,20 @@
 
 
 if __name__ == '__main__':
-    A = tuple(map(int, input().split()))
+    a = tuple(map(int, input().split()))
     i = 0
-    for index, el in enumerate(A):
-        k = A.count(el)
+    for index, el in enumerate(a):
+        k = a.count(el)
         if k == 2:
-            if len(A) > index + 1:
-                if A[index + 1] == el:
+            if len(a) > index + 1:
+                if a[index + 1] == el:
                     i = index + 1
                     break
                 else:
                     continue
     if i:
-        for index, el in enumerate(A):
-            if i < index < len(A):
+        for index, el in enumerate(a):
+            if i < index < len(a):
                 print(el, end=' ')
     else:
         print("There are no pairs of similar adjacent elements!")
